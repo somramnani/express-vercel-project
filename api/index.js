67d31affa6bac520a69f69374cd1d43d1ecbc75f
somramnani <som.ramnani@gmail.com>
2024-11-18 15:06:03 -0500

    Add api route to fetch data from super-hero-api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+
 const app = express();
 
 app.use(cors());
@@ -7,6 +9,21 @@ app.use(cors());
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.get("/hello", (req, res) => res.json({ message: "hello" }));
+
+app.get("/super-hero-api/:data", async (req, res) => {
+  // res.send('This has CORS enabled 🎈')
+
+  await axios({
+    method: "get",
+    url: `https://superheroapi.com/api/10161414156754853/search/${req.params.data}`,
+  })
+    .then(function (response) {
+      return res.send(response.data);
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
+});
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
