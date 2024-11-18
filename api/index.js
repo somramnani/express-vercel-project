e33d3a17ea4af195a072eb09882ec5562f16695b
somramnani <som.ramnani@gmail.com>
2024-11-18 18:49:13 -0500

    Add Cors fix

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const axios = require("axios");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3001", // Replace with your frontend URL in production
+  origin: "http://localhost:3000", // Replace with your frontend URL in production
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   credentials: true, // Allow cookies if needed
 };
 
-app.options("*", cors(corsOptions)); // Preflight requests for all routes
+// app.options("*", cors(corsOptions)); // Preflight requests for all routes
+
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
@@ -21,9 +23,10 @@ app.get("/super-hero-api/:data", async (req, res) => {
 
   await axios({
     method: "get",
-    url: `https://superheroapi.com/api/10161414156754853/search/${req.params.data}`,
+    url: `https://superheroapi.com/api/88fa1b97046d4d9666b719510bbb8257/search/${req.params.data}`,
   })
     .then(function (response) {
+      console.log(response.data);
       return res.send(response.data);
     })
     .catch((err) => {
